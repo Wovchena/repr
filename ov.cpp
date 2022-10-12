7da6784ffefc9e9acc8571d657fd1f77137c5ca0
Wovchena <vladimir.zlobin@intel.com>
2022-10-12 17:39:54 +0400

    Temproary ov::Core

--- ov.cpp
+++ ov.cpp
@@ -1,29 +1,15 @@
-#include <opencv2/opencv.hpp>
+#include <openvino/openvino.hpp>
+// #include <opencv2/opencv.hpp>
 using namespace std;
 
-static inline void catcher() noexcept {
-    if (std::current_exception()) {
-        try {
-            std::rethrow_exception(std::current_exception());
-        } catch (const std::exception& error) {
-            cerr << error.what() << endl;
-        } catch (...) {
-            cerr << "Non-exception object thrown" << endl;
-        }
-        std::exit(1);
+int main(int argc, char* argv[]) {
+    // ov::Core core;
+    ov::CompiledModel compiled_model = ov::Core{}.compile_model(argv[1], "CPU");
+    try {
+        compiled_model.get_property(ov::optimal_number_of_infer_requests);
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what();
+        return 1;
     }
-    std::abort();
-}
-
-int main() {
-    set_terminate(catcher);
-    auto start = chrono::steady_clock::now();
-    for (int i = 0; i < 1000; ++i) {
-        // cv::imshow("", cv::Mat(cv::Size{1280, 720}, CV_8UC3, cv::Scalar{0, 0, 0}));
-        cv::imshow("", cv::Mat(cv::Size{300, 300}, CV_8UC3, cv::Scalar{0, 0, 0}));
-        cv::waitKey(1);
-    }
-    auto end = chrono::steady_clock::now();
-    cout << double((end - start).count()) / 1000 / 1000 << '\n';
     return 0;
 }
