8a2b532757edd61141e85aac170f3cbcc3a56bdb
Wovchena <vladimir.zlobin@intel.com>
2023-06-07 17:42:07 +0400

    Python openvino corrupts data

--- ov.cpp
+++ ov.cpp
@@ -1,15 +1,46 @@
 #include <openvino/openvino.hpp>
-// #include <opencv2/opencv.hpp>
+#include "openvino/opsets/opset11.hpp"
+#include <opencv2/opencv.hpp>
+
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    // ov::Core core;
-    ov::CompiledModel compiled_model = ov::Core{}.compile_model(argv[1], "CPU");
-    try {
-        compiled_model.get_property(ov::optimal_number_of_infer_requests);
-    } catch (const std::exception& ex) {
-        std::cerr << ex.what();
-        return 1;
+float absdiff(const cv::Mat ref, ov::Tensor pred) {
+    float diff = 0.0f;
+    float* ref_data = (float*)(ref.data);
+    float* pred_data = pred.data<float>();
+    for (size_t i = 0; i < pred.get_size(); ++i) {
+        diff = max(std::abs(ref_data[i] - pred_data[i]), diff);
+    }
+    return diff;
+}
+
+int main() {
+    // identity
+    ov::Shape input_shape{1, 20, 20, 3};
+    auto param_node = make_shared<ov::op::v0::Parameter>(ov::element::f32, input_shape);
+    ov::Core core;
+    shared_ptr<ov::Model> model = make_shared<ov::Model>(param_node, ov::ParameterVector{param_node});
+    ov::CompiledModel identity = core.compile_model(model, "CPU");
+    ov::InferRequest req = identity.create_infer_request();
+    cv::Mat inp{cv::Size(20, 20), CV_32FC3};
+    cv::randu(inp, 0.0, 255.0);
+    req.set_input_tensor(ov::Tensor{ov::element::f32, input_shape, inp.data});
+    req.infer();
+    if (0.0f != absdiff(inp, req.get_output_tensor())) {
+        cerr << "identity diff\n";
+        exit(1);
+    }
+    // scale
+    ov::preprocess::PrePostProcessor ppp{model};
+    ppp.input().preprocess().scale({255.0f});
+    model = ppp.build();
+    ov::CompiledModel scaler = core.compile_model(model, "CPU");
+    req = scaler.create_infer_request();
+    req.set_input_tensor(ov::Tensor{ov::element::f32, input_shape, inp.data});
+    req.infer();
+    if (0.0f != absdiff(inp / 255.0f, req.get_output_tensor())) {
+        cerr << "scale diff\n";
+        exit(1);
     }
     return 0;
 }
--- repr.py
+++ repr.py
@@ -1,22 +1,27 @@
 import openvino.runtime as ov
-import sys
+from openvino.preprocess import PrePostProcessor
+import numpy as np
 
 
 def main():
-    print('OpenVINO:')
-    print(f"{'Build ':.<39} {ov.get_version()}")
-    if len(sys.argv) != 3:
-        print(f'Usage: {sys.argv[0]} <path_to_model>, <device>')
-        return 1
+    # identity
+    input_shape = [1, 20, 20, 3]
+    param_node = ov.op.Parameter(ov.Type.f32, ov.Shape(input_shape))
     core = ov.Core()
-    model = core.read_model(sys.argv[1])
-    compiled_model = core.compile_model(model, sys.argv[2])
-    keys = compiled_model.get_property('SUPPORTED_PROPERTIES')
-    print("Model:")
-    for k in keys:
-        if k not in ('SUPPORTED_METRICS', 'SUPPORTED_CONFIG_KEYS', 'SUPPORTED_PROPERTIES'):
-            print(k)
-            print(f'  {k}: {compiled_model.get_property(k)}')
+    model = ov.Model(param_node, [param_node])
+    identity = core.compile_model(model, "CPU")
+    inp = np.random.random(input_shape) * 224.0 + np.random.random(input_shape)
+    pred = next(iter(identity({0: inp}).values()))
+    print(np.abs(inp - pred).max())  # non zero
+    # scale
+    aligned_inp = pred  # Assume openvino currupts input always in the same way
+    ppp = PrePostProcessor(model)
+    ppp.input().preprocess().scale([255.0])
+    model = ppp.build()
+    scaler = core.compile_model(model, "CPU")
+    scaled_pred = next(iter(scaler({0: inp}).values()))
+    scaled_ref = aligned_inp / 255.0
+    print(np.abs(scaled_pred - scaled_ref).max())  # non zero
 
 
 if __name__ == '__main__':
