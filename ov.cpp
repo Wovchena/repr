10400380e660d79e0fca52673a0978c1bb185c58
Wovchena <vladimir.zlobin@intel.com>
2023-08-09 19:59:16 +0400

    iGPU FPS depends on how CompiledModel is constructed. Tested openvino commit a84664a0668e03253123f1754acb32c9d31ea23b

--- ov.cpp
+++ ov.cpp
@@ -6,9 +6,15 @@ using namespace std;
 
 int main() {
     ov::Core core;
-    auto model = core.read_model("/home/wov/.cache/omz/intel/semantic-segmentation-adas-0001/FP16/semantic-segmentation-adas-0001.xml");
-    ov::preprocess::PrePostProcessor ppp(model);
-    ppp.output().tensor().set_element_type(ov::element::f32);
-    model = ppp.build();
-    core.compile_model(model, "GPU");
+    auto model = core.read_model("/home/wov/.cache/omz/intel/person-reidentification-retail-0288/FP16/person-reidentification-retail-0288.xml");
+    ov::CompiledModel compiled = core.compile_model(model, "GPU", {  // Replace ov::Model with string to see speed up
+        {ov::hint::performance_mode(ov::hint::PerformanceMode::THROUGHPUT)},
+        {ov::hint::num_requests(3)}  // Set num_requests(2) to see speed up
+    });
+    ov::InferRequest req = compiled.create_infer_request();
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; ; ++i) {
+        req.infer();
+        std::cout << "FPS: " << i * 1e9 / std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start).count() << '\n';
+    }
 }
