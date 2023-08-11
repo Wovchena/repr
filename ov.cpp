569996d8cb6bfc0b2157ba9ea79bfe5cb7403c32
Wovchena <vladimir.zlobin@intel.com>
2023-08-11 15:38:17 +0400

    smart_classroom_demo/cpp_gapi error
    ./intel64/Release/smart_classroom_demo_gapi -i '/home/wov/Videos/classroom.mp4' -m_act ~/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.xml -student_ac sitting,writing,raising_hand,standing,turned_around,lie_on_the_desk
    fails with
    double free or corruption (out)
    
    I also saw an error
    InferRequest for model: /home/wov/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.xml finished with InferenceEngine::StatusCode: -1
    
    This reproducer gives the second error. Tested OpenVINO commit 2a3132941c39a4eff645b33ddd82bb0a23a293e8, OpenCV commit 1794cdc03c9505bb46f33a5cde5e210c1c7f65a4

--- ov.cpp
+++ ov.cpp
@@ -2,19 +2,70 @@
 #include "openvino/opsets/opset11.hpp"
 #include <opencv2/opencv.hpp>
 
+#include <opencv2/gapi/cpu/gcpukernel.hpp>
+#include <opencv2/gapi/gcommon.hpp>
+#include <opencv2/gapi/util/optional.hpp>
+#include <opencv2/gapi/core.hpp>
+#include <opencv2/gapi/garg.hpp>
+#include <opencv2/gapi/garray.hpp>
+#include <opencv2/gapi/gcommon.hpp>
+#include <opencv2/gapi/gcomputation.hpp>
+#include <opencv2/gapi/gmat.hpp>
+#include <opencv2/gapi/gopaque.hpp>
+#include <opencv2/gapi/gproto.hpp>
+#include <opencv2/gapi/gscalar.hpp>
+#include <opencv2/gapi/gstreaming.hpp>
+#include <opencv2/gapi/infer.hpp>
+#include <opencv2/gapi/infer/parsers.hpp>
+#include <opencv2/gapi/render/render.hpp>
+#include <opencv2/gapi/streaming/format.hpp>
+#include <opencv2/gapi/garray.hpp>
+#include <opencv2/gapi/gkernel.hpp>
+#include <opencv2/gapi/gmat.hpp>
+#include <opencv2/gapi/gopaque.hpp>
+#include <opencv2/gapi/render/render_types.hpp>
+#include <opencv2/gapi/infer/ie.hpp>
+#include <opencv2/gapi/infer/parsers.hpp>
+
 using namespace std;
 
+using PAInfo = std::tuple<cv::GMat, cv::GMat, cv::GMat, cv::GMat, cv::GMat, cv::GMat, cv::GMat>;
+G_API_NET(PersonDetActionRec, <PAInfo(cv::GMat)>, "person-detection-action-recognition");
+
+cv::GRunArgs generator(const cv::GTypesInfo& info) {
+    return std::vector<cv::GRunArg>{cv::GRunArg(cv::Mat{cv::Size(1920, 180), CV_8UC3})};
+}
+
 int main() {
-    ov::Core core;
-    auto model = core.read_model("/home/wov/.cache/omz/intel/person-reidentification-retail-0288/FP16/person-reidentification-retail-0288.xml");
-    ov::CompiledModel compiled = core.compile_model(model, "GPU", {  // Replace ov::Model with string to see speed up
-        {ov::hint::performance_mode(ov::hint::PerformanceMode::THROUGHPUT)},
-        {ov::hint::num_requests(3)}  // Set num_requests(2) to see speed up
-    });
-    ov::InferRequest req = compiled.create_infer_request();
-    auto start = std::chrono::steady_clock::now();
-    for (int i = 0; ; ++i) {
-        req.infer();
-        std::cout << "FPS: " << i * 1e9 / std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - start).count() << '\n';
-    }
+    const std::array<std::string, 7> action_detector_6 = {"ActionNet/out_detection_loc",
+                                                            "ActionNet/out_detection_conf",
+                                                            "ActionNet/action_heads/out_head_1_anchor_1",
+                                                            "ActionNet/action_heads/out_head_2_anchor_1",
+                                                            "ActionNet/action_heads/out_head_2_anchor_2",
+                                                            "ActionNet/action_heads/out_head_2_anchor_3",
+                                                            "ActionNet/action_heads/out_head_2_anchor_4"};
+    auto action_net =
+        cv::gapi::ie::Params<PersonDetActionRec>{
+            // https://storage.openvinotoolkit.org/repositories/open_model_zoo/2023.0/models_bin/1/person-detection-action-recognition-0006/FP16
+            "/home/wov/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.xml",
+            "/home/wov/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.bin",
+            "CPU",
+        }.cfgOutputLayers(action_detector_6);
+    cv::gapi::GNetPackage networks;
+    networks+= cv::gapi::networks(action_net);
+
+    cv::GMat in;
+    cv::GMat location, detect_confidences, priorboxes, action_con1, action_con2, action_con3, action_con4;
+    std::tie(location, detect_confidences, priorboxes, action_con1, action_con2, action_con3, action_con4) =
+        cv::gapi::infer<PersonDetActionRec>(in);
+    auto outs = GOut(location);
+
+    cv::GComputation pipeline(cv::GIn(in), std::move(outs));
+    cv::GStreamingCompiled stream =
+            pipeline.compileStreaming(cv::compile_args(cv::gapi::kernels<>(), networks));
+    stream.setSource(cv::detail::ExtractArgsCallback{generator});
+    stream.start();
+    cv::Mat location_mat;
+    stream.pull(cv::gout(location_mat));  // InferRequest for model: /home/wov/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.xml finished with InferenceEngine::StatusCode: -1
+    // I also encountered error "double free or corruption (out)"
 }
