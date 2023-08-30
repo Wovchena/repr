888f117ebd64b1fd78f6fed75f3eeb79c4669464
Wovchena <vladimir.zlobin@intel.com>
2023-08-30 14:56:30 +0400

    scaler

--- ov.cpp
+++ ov.cpp
@@ -1,71 +1,20 @@
 #include <openvino/openvino.hpp>
-#include "openvino/opsets/opset11.hpp"
+#include <openvino/opsets/opset11.hpp>
 #include <opencv2/opencv.hpp>
-
-#include <opencv2/gapi/cpu/gcpukernel.hpp>
-#include <opencv2/gapi/gcommon.hpp>
-#include <opencv2/gapi/util/optional.hpp>
-#include <opencv2/gapi/core.hpp>
-#include <opencv2/gapi/garg.hpp>
-#include <opencv2/gapi/garray.hpp>
-#include <opencv2/gapi/gcommon.hpp>
-#include <opencv2/gapi/gcomputation.hpp>
-#include <opencv2/gapi/gmat.hpp>
-#include <opencv2/gapi/gopaque.hpp>
-#include <opencv2/gapi/gproto.hpp>
-#include <opencv2/gapi/gscalar.hpp>
-#include <opencv2/gapi/gstreaming.hpp>
-#include <opencv2/gapi/infer.hpp>
-#include <opencv2/gapi/infer/parsers.hpp>
-#include <opencv2/gapi/render/render.hpp>
-#include <opencv2/gapi/streaming/format.hpp>
-#include <opencv2/gapi/garray.hpp>
-#include <opencv2/gapi/gkernel.hpp>
-#include <opencv2/gapi/gmat.hpp>
-#include <opencv2/gapi/gopaque.hpp>
-#include <opencv2/gapi/render/render_types.hpp>
-#include <opencv2/gapi/infer/ie.hpp>
-#include <opencv2/gapi/infer/parsers.hpp>
 
 using namespace std;
 
-using PAInfo = std::tuple<cv::GMat, cv::GMat, cv::GMat, cv::GMat, cv::GMat, cv::GMat, cv::GMat>;
-G_API_NET(PersonDetActionRec, <PAInfo(cv::GMat)>, "person-detection-action-recognition");
-
-cv::GRunArgs generator(const cv::GTypesInfo& info) {
-    return std::vector<cv::GRunArg>{cv::GRunArg(cv::Mat{cv::Size(1920, 180), CV_8UC3})};
-}
-
 int main() {
-    const std::array<std::string, 7> action_detector_6 = {"ActionNet/out_detection_loc",
-                                                            "ActionNet/out_detection_conf",
-                                                            "ActionNet/action_heads/out_head_1_anchor_1",
-                                                            "ActionNet/action_heads/out_head_2_anchor_1",
-                                                            "ActionNet/action_heads/out_head_2_anchor_2",
-                                                            "ActionNet/action_heads/out_head_2_anchor_3",
-                                                            "ActionNet/action_heads/out_head_2_anchor_4"};
-    auto action_net =
-        cv::gapi::ie::Params<PersonDetActionRec>{
-            // https://storage.openvinotoolkit.org/repositories/open_model_zoo/2023.0/models_bin/1/person-detection-action-recognition-0006/FP16
-            "/home/wov/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.xml",
-            "/home/wov/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.bin",
-            "CPU",
-        }.cfgOutputLayers(action_detector_6);
-    cv::gapi::GNetPackage networks;
-    networks+= cv::gapi::networks(action_net);
-
-    cv::GMat in;
-    cv::GMat location, detect_confidences, priorboxes, action_con1, action_con2, action_con3, action_con4;
-    std::tie(location, detect_confidences, priorboxes, action_con1, action_con2, action_con3, action_con4) =
-        cv::gapi::infer<PersonDetActionRec>(in);
-    auto outs = GOut(location);
-
-    cv::GComputation pipeline(cv::GIn(in), std::move(outs));
-    cv::GStreamingCompiled stream =
-            pipeline.compileStreaming(cv::compile_args(cv::gapi::kernels<>(), networks));
-    stream.setSource(cv::detail::ExtractArgsCallback{generator});
-    stream.start();
-    cv::Mat location_mat;
-    stream.pull(cv::gout(location_mat));  // InferRequest for model: /home/wov/.cache/omz/intel/person-detection-action-recognition-0006/FP16/person-detection-action-recognition-0006.xml finished with InferenceEngine::StatusCode: -1
-    // I also encountered error "double free or corruption (out)"
+    float DIVIDEND = 0x1.b7f1200000000p+0;
+    float DIVISOR = 0x1.8000000000000p+0;  // 1.5
+    std::cout << std::hexfloat << DIVIDEND << ' ' << DIVISOR << '\n';  // Make sure it's unchanged
+    auto param_node = make_shared<ov::op::v0::Parameter>(ov::element::f32, ov::Shape{1});
+    shared_ptr<ov::Model> model = make_shared<ov::Model>(param_node, ov::ParameterVector{param_node});
+    ov::preprocess::PrePostProcessor ppp{model};
+    ppp.input().preprocess().scale({DIVISOR});
+    ov::CompiledModel scaler = ov::Core{}.compile_model(ppp.build(), "CPU");
+    ov::InferRequest req = scaler.create_infer_request();
+    req.get_input_tensor().data<float>()[0] = DIVIDEND;
+    req.infer();
+    std::cout << std::hexfloat << (DIVIDEND / DIVISOR) << ' ' << req.get_output_tensor().data<float>()[0] << '\n';
 }
--- repr.py
+++ repr.py
@@ -1,27 +1,24 @@
-import cv2
-from openvino.model_api.models import ClassificationModel
-
-
-def simple():
-    model = ClassificationModel.create_model("resnet-18-pytorch", device="CPU")
-    print(model(cv2.imread("/home/wov/Pictures/dog.jpg")))  # https://storage.openvinotoolkit.org/test_data/images/dog.jpg
-    print(model(cv2.imread("/home/wov/Pictures/cat.jpg")))  # https://storage.openvinotoolkit.org/test_data/images/cat.jpg
-
-
-def infer_sync():
-    model = ClassificationModel.create_model("resnet-18-pytorch", device="CPU")
-    dict_data, input_meta = model.preprocess(cv2.imread("/home/wov/Pictures/dog.jpg"))
-    raw_result = model.inference_adapter.infer_sync(dict_data)
-    print(model.postprocess(raw_result, input_meta))
-    dict_data, input_meta = model.preprocess(cv2.imread("/home/wov/Pictures/cat.jpg"))
-    raw_result = model.inference_adapter.infer_sync(dict_data)
-    print(model.postprocess(raw_result, input_meta))
+import openvino.runtime as ov
+from openvino.preprocess import PrePostProcessor
+import numpy as np
 
 
 def main():
-    simple()
-    infer_sync()
+    DIVIDEND = float.fromhex('0x1.b7f1200000000p+0')
+    NP_DIVIDEND = np.array([DIVIDEND], np.float32)
+    DIVISOR = float.fromhex('0x1.8000000000000p+0')  # 1.5
+    NP_DIVISOR = np.array([DIVISOR], np.float32)
+    assert DIVIDEND.hex() == NP_DIVIDEND.item().hex()
+    assert DIVISOR.hex() == NP_DIVISOR.item().hex()
+    param_node = ov.op.Parameter(ov.Type.f32, ov.Shape([1]))
+    model = ov.Model(param_node, [param_node])
+    ppp = PrePostProcessor(model)
+    ppp.input().preprocess().scale(NP_DIVISOR)
+    scaler = ov.Core().compile_model(ppp.build(), "CPU")
+    scaled_pred = scaler({0: NP_DIVIDEND})[0]
+    print((NP_DIVIDEND / NP_DIVISOR).item().hex(), scaled_pred.item().hex())
+    assert (NP_DIVIDEND / NP_DIVISOR).item().hex() == scaled_pred.item().hex()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
