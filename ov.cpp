534b487756445af4b90dbf3659a02ada625ae22b
Vladimir Zlobin <vladimir.zlobin@intel.com>
2025-03-15 12:25:01 +0400

    warnings

--- ov.cpp
+++ ov.cpp
@@ -1,13 +1,15 @@
 #include <openvino/openvino.hpp>
-// A command to remind myself how to disassemble after compiling Release version but link with OV Debug because I usually build OV in Debug. -ggdb for debug symbols but -O3 and -DNDEBUG for Release:
-// g++ -ggdb -O3 -DNDEBUG -Xlinker -rpath -Xlinker /home/vzlobin/r/v/bin/intel64/Debug --include-directory /home/vzlobin/r/v/src/core/include/ --include-directory /home/vzlobin/r/v/src/inference/include/ ov.cpp /home/vzlobin/r/v/bin/intel64/Debug/libopenvino.so.2025.1.0 && gdb -ex 'set pagination off' -ex 'disassemble /s main' --args ./a.out arg
+// A command to remind myself how to disassemble after compiling Release version but link with OV Debug because I usually build OV in Debug. -ggdb for debug symbols but -O3 and -DNDEBUG for Release: Treat headers as system's to suppress their warnings.
+// g++ -ggdb -O3 -DNDEBUG -Wall -pedantic -Werror -Xlinker -rpath -Xlinker /home/vzlobin/r/v/bin/intel64/Debug -isystem /home/vzlobin/r/v/src/core/include/ -isystem /home/vzlobin/r/v/src/inference/include/ ov.cpp /home/vzlobin/r/v/bin/intel64/Debug/libopenvino.so.2025.1.0 && gdb -ex 'set pagination off' -ex 'disassemble /s main' --args ./a.out arg
 int main(int argc, char* argv[]) {
     using namespace std;
     volatile size_t vector_size = 0;
     vector<int> vector(vector_size);
+    volatile size_t do_not_optimize_away = 0;
     for (size_t idx = 0; idx < vector.size(); ++idx) {
-        volatile size_t do_not_optimize_away = 0;
+        do_not_optimize_away = idx;
     }
+    std::cout << do_not_optimize_away << '\n';
     // ov::Tensor tensor{ov::element::f32, {0}};
     // struct TensorIterator {
     //     size_t idx;
